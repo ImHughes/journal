3b9a68209d65ed58d9157da5b20ab5ad575fa649
Hughes Speer <ImHughes@users.noreply.github.com>
2019-04-12 13:59:17 -0500

    fixed

---
+++ src/scripts/journal.js
@@ -0,0 +1,30 @@
+const objectsJournalEnter = {
+    date: "04/10/2019",
+    Concepts: "html",
+    entry: "I learned html today",
+    mood: "happy"
+ };
+ 
+ const JournalEntry = [
+    {
+        date: "04/11/2019",
+        Concepts: "css",
+        entry: "I learned css today",
+        mood: "normal"
+    },
+    {
+        date: "04/12/2019",
+        Concepts: "javascripts",
+        entry: "I learned javascripts today",
+        mood: "sad"
+    },
+    {
+        date: "04/13/2019",
+        Concepts: "objects",
+        entry: "I learned objects today",
+        mood: "happy"
+    }
+ ]
+ 
+ JournalEntry.push();
+ console.log("JournalEntry", JournalEntry);
